6e654d127bb75132916c334d1bf8bf6e5eaafb8f
sagar pathak <pathaksagar16@gmail.com>
2021-10-03 15:49:52 -0700

    first commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	float anualsal;
+	cout << "Please enter your anual Salary: ";
+	cin >> anualsal;
+	float monthlysal = anualsal / 12;
+	cout << "Your monthly salary is: " << monthlysal << endl;
+	float tenyear = anualsal * 10;
+	cout << "Your salary will be " << tenyear << " in 10 years";
+	
+	system("pause > 0");
+	//return 0;
+}
