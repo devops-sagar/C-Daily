53a0314d2abba90d427e29d4c8d6f0ae2f90bb8f
sagar pathak <pathaksagar16@gmail.com>
2021-10-04 22:53:44 -0700

    created cipher program

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,13 +3,22 @@ using namespace std;
 
 int main() {
 	
-	float anualsal;
-	cout << "Please enter your anual Salary: ";
-	cin >> anualsal;
-	float monthlysal = anualsal / 12;
-	cout << "Your monthly salary is: " << monthlysal << endl;
-	float tenyear = anualsal * 10;
-	cout << "Your salary will be " << tenyear << " in 10 years";
+	//float anualsal;
+	//cout << "Please enter your anual Salary: ";
+	//cin >> anualsal;
+	//float monthlysal = anualsal / 12;
+	//cout << "Your monthly salary is: " << monthlysal << endl;
+	//float tenyear = anualsal * 10;
+	//cout << "Your salary will be " << tenyear << " in 10 years";
+
+	/*Program to cipher or encoding words into the ASCII value*/
+	char c1, c2, c3, c4, c5;
+	cout << "Enter the 5 letter word to encode: ";
+	cin >> c1 >> c2 >> c3 >> c4 >> c5;
+	cout << "Encoded Message: " << int(c1) << int(c2) << int(c3) << int(c4) << int(c5);
+
+
+
 	
 	system("pause > 0");
 	//return 0;
