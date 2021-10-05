853c8340cb46b70a04df652b4a390bfeaa8c35c5
sagar pathak <pathaksagar16@gmail.com>
2021-10-04 23:09:58 -0700

    Cipher Decipher program

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -11,11 +11,22 @@ int main() {
 	//float tenyear = anualsal * 10;
 	//cout << "Your salary will be " << tenyear << " in 10 years";
 
+	//********************************************************************************************
+
 	/*Program to cipher or encoding words into the ASCII value*/
 	char c1, c2, c3, c4, c5;
 	cout << "Enter the 5 letter word to encode: ";
 	cin >> c1 >> c2 >> c3 >> c4 >> c5;
-	cout << "Encoded Message: " << int(c1) << int(c2) << int(c3) << int(c4) << int(c5);
+	cout << "Encoded Message: " << int(c1) << " " << int(c2) << " " << int(c3) << " " << int(c4) << " " << int(c5) << endl;
+
+	int n1, n2, n3, n4, n5;
+	cout << "Enter the ASCII values for five letter to decode: ";
+	cin >> n1 >> n2 >> n3 >> n4 >> n5;
+	cout << "Decoded Message: " << char(n1) << char(n2) << char(n3) << char(n4) << char(n5);
+
+	//********************************************************************************************
+
+
 
 
 
